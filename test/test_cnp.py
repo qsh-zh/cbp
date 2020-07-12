c87a8d0d53604875564df3cf00a450e075f4f0b7
zqsh <zqsh19970218@gmail.com>
2020-07-12 17:26:19 -0400

    fix seed

--- test/test_cnp.py
+++ test/test_cnp.py
@@ -9,10 +9,11 @@ from .utils import sinkhorn_bp_equal
 
 class TestGraph(unittest.TestCase):
     def test_hmm_policy(self):
+        rng = np.random.RandomState(1)
         for i in range(5):
             for policy in [bp_policy, avg_policy, factor_policy]:
-                num_node = int(np.random.randint(2, 6))
-                node_dim = int(np.random.randint(4, 6))
+                num_node = int(rng.randint(2, 6))
+                node_dim = int(rng.randint(4, 6))
                 print(
                     f"Run {i}-th experiment, {num_node} nodes with {node_dim} status")
                 self.graph = HMMBuilder(num_node, node_dim, policy)()
@@ -21,10 +22,11 @@ class TestGraph(unittest.TestCase):
                 self.assertTrue(all(sinkhorn_bp_equal(self.graph, num_node)))
 
     def test_line_policy(self):
+        rng = np.random.RandomState(1)
         for i in range(5):
             for policy in [bp_policy, avg_policy, factor_policy]:
-                num_node = int(np.random.randint(2, 4))
-                node_dim = int(np.random.randint(2, 5))
+                num_node = int(rng.randint(2, 4))
+                node_dim = int(rng.randint(2, 5))
                 print(
                     f"Run {i}-th experiment, {num_node} nodes with {node_dim} status")
                 self.graph = LineBuilder(num_node, node_dim, avg_policy)()
