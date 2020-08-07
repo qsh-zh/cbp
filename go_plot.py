4fb1d9a73d1a2406d8511c9a2f831933890d85cf
zqsh <zqsh19970218@gmail.com>
2020-08-07 15:24:45 -0400

    clean plot

--- go_plot.py
+++ go_plot.py
@@ -9,20 +9,6 @@ cmds = ppk.param_sweep(
     hmm_length=range(2, 100, 2),
     seed=range(10))
 
-data = {"go_gaussian": {}}
-
-for item in cmds:
-    parser = cmdhelper.cfg_from_str(item)
-    if parser.hmm_length not in data["go_gaussian"]:
-        data["go_gaussian"][parser.hmm_length] = []
-
-    executor = GOBenchExecutor.load(
-        f"{parser.pkl_path()}/{parser.pkl_name()}.pkl")
-    data["go_gaussian"][parser.hmm_length].append(executor.exp_record["step"])
-
-ax = ppk.meanstd_plot(data, {"go_gaussian": ""}, is_label=False)
-ax.figure.savefig('test.png')
-
 
 def draw(cmds, exp_vary, metric, fig_name):
     data = {"go_gaussian": {}}
@@ -42,3 +28,4 @@ def draw(cmds, exp_vary, metric, fig_name):
 
 
 draw(cmds, 'hmm_length', 'step', 'length_step.png')
+draw(cmds, 'hmm_length', 'time', 'length_step.png')
