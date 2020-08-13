fdd22e8f3a7e208dc82360ffc7c2f21615a40930
zqsh <zqsh19970218@gmail.com>
2020-08-12 23:24:29 -0400

    add multiple guassian hmm

--- cbp/builder/go_simulator.py
+++ cbp/builder/go_simulator.py
@@ -1,4 +1,6 @@
-from .base_simulator import TrajSimulator, TrajData, PotentialType
+import numpy as np
+
+from .base_simulator import PotentialType, TrajData, TrajSimulator
 
 
 class ContinuousObserData(TrajData):
@@ -9,7 +11,7 @@ class ContinuousObserData(TrajData):
     @sensor.setter
     def sensor(self, sensor_data):
         assert sensor_data.shape[1] == self.time_step
-        dict.__setitem__(self, "fix_margin", sensor_data.T)
+        dict.__setitem__(self, "fix_margin", np.swapaxes(sensor_data, 0, 1))
 
 
 class GOHMMSimulator(TrajSimulator):
@@ -27,3 +29,31 @@ class GOHMMSimulator(TrajSimulator):
     def observe(self, state):
         return self.rng.normal(loc=self.record[PotentialType.EMISSION]["loc"][state],
                                scale=self.record[PotentialType.EMISSION]["scale"][state])
+
+
+class GMOHMMSimulator(TrajSimulator):
+    def __init__(self, record, random_seed, is_theorem=False):
+        super().__init__(record, random_seed, is_theorem=is_theorem)
+
+    def register_potential(self, ptype, potential):
+        if ptype == PotentialType.EMISSION:
+            if potential["loc"].ndim > 1:
+                setattr(self, "obser_dim", potential["loc"].shape[1])
+            else:
+                setattr(self, "obser_dim", 1)
+            assert potential["loc"].shape[0] == self.record.state_num
+            assert potential["scale"].shape[0] == self.record.state_num
+
+        super().register_potential(ptype, potential)
+
+    def observe_traj(self, traj):
+        rtn = np.zeros_like((traj.shape[0], traj.shape[1], self.obser_dim),
+                            traj, dtype=float)
+        loop_iter = np.nditer(traj, flags=['multi_index'])
+        for i in loop_iter:
+            rtn[loop_iter.multi_index] = self.observe(i)
+        return rtn
+
+    def observe(self, state):
+        return self.rng.multivariate_normal(mean=self.record[PotentialType.EMISSION]["loc"][state],
+                                            cov=self.record[PotentialType.EMISSION]["scale"][state])
